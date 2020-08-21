e28f0176446677cc2a3b59a99cb58449dd71e304
Mani Manavalan <Mani.Manavalan@non.se.com>
2020-08-21 00:10:43 -0400

    hello world base

---
+++ src/main/java/com/stac/simplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.stac.simplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+	//SimpleMethod
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld")
+	public String helloWorld()
+	{
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+	
+		return new UserDetails("Mani" , "M", "Boston");
+	}
+}
+

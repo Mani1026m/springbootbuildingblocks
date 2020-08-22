b01058997fa17544e000650a134e4edab6feca67
Mani Manavalan <Mani.Manavalan@non.se.com>
2020-08-22 00:18:02 -0400

    JPA Rest base services development complete.

---
+++ src/main/java/com/stac/simplify/restservices/controllers/UserController.java
@@ -0,0 +1,69 @@
+package com.stac.simplify.restservices.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stac.simplify.restservices.entities.User;
+import com.stac.simplify.restservices.services.UserService;
+
+//Controller
+@RestController
+public class UserController {
+	
+	//Autowire the user service.
+	
+	@Autowired
+	private UserService userService;
+	
+	//getAllUser method here.
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+	
+	//Create User
+	//@RequestBody Annotation
+	//PostMapping annotation
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+
+	
+	//getUserById
+	@GetMapping("/users/{id}")
+	public Optional<User> getUsersById(@PathVariable("id") Long id){
+		return userService.getUserById(id);
+	}
+	
+	//UpdateUserById
+	@PutMapping("/users/{id}")
+	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
+		
+		return userService.updateUserById(id, user);
+	}
+	
+	//deleteUserById
+	@DeleteMapping("/users/{id}")
+	public void deleteUserById(@PathVariable("id") Long id) {
+		userService.deleteUserById(id);
+	}
+	
+	//getUserByUsername
+	@GetMapping("/users/byusername/{username}")
+	public User getUserByUsername(@PathVariable("username") String username) {
+		return userService.getUserByUsername(username);
+	}
+	
+}
---
+++ src/main/java/com/stac/simplify/restservices/services/UserService.java
@@ -0,0 +1,59 @@
+package com.stac.simplify.restservices.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.stac.simplify.restservices.entities.User;
+import com.stac.simplify.restservices.repositories.UserRepository;
+
+//Service
+@Service
+public class UserService {
+	
+	//Autowire the user repository
+	@Autowired
+	private UserRepository userRepository;
+
+	//getAllUsers method
+	public List<User> getAllUsers(){
+	
+		return userRepository.findAll();
+	}
+	
+	
+	//CreateUserMethod
+	
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	//Get user by ID
+	
+	public Optional<User> getUserById(Long id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		return user;
+	}
+	
+	//UpdateUserById
+	
+	public User updateUserById(Long id, User user) {
+		user.setId(id);
+		return userRepository.save(user);
+	}
+	
+	//deleteUserById
+	public void deleteUserById(Long id) {
+		if(userRepository.findById(id).isPresent()) {
+			userRepository.deleteById(id);
+			
+		}
+	}
+	
+	//getUserByUsername
+	
+	public User getUserByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+}
